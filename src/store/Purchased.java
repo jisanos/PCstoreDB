1e3a3bd50a4a7ffc2daf77c19ba5fcce0f18befa
jisanos <44531911+jisanos@users.noreply.github.com>
2019-05-02 14:33:52 -0400

    Purchased tab GUI now displays what the user has bought

--- src/store/Purchased.java
+++ src/store/Purchased.java
@@ -1,7 +1,62 @@
 package store;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class Purchased {
 
+	private StringProperty NAME = new SimpleStringProperty();
 	
+	private StringProperty SPECS = new SimpleStringProperty();
+	
+	private IntegerProperty PRICE= new SimpleIntegerProperty();
+	
+	private IntegerProperty QUANTITY = new SimpleIntegerProperty();
+	
+	public Purchased(String name, String specs, int price, int quantity)
+	{
+		this.setNAME(name);
+		
+		this.setSPECS(specs);
+		
+		this.setPRICE(price);
+		
+		this.setQUANTITY(quantity);
+		
+		
+	}
+////////////////////////////////////////	
+	public String getNAME() {
+		return NAME.get();
+	}
+
+	public void setNAME(String nAME) {
+		NAME.set(nAME);
+	}
+//////////////////////////////////////////
+	public String getSPECS() {
+		return SPECS.get();
+	}
+
+	public void setSPECS(String sPECS) {
+		SPECS.set(sPECS);
+	}
+/////////////////////////////////
+	public int getPRICE() {
+		return PRICE.get();
+	}
+	public void setPRICE(int pRICE) {
+		PRICE.set(pRICE);
+	}
+///////////////////////////////
+	public int getQUANTITY() {
+		return QUANTITY.get();
+	}
+	public void setQUANTITY(int qUANTITY)
+	{
+		QUANTITY.set(qUANTITY);
+	}
 	
 }
